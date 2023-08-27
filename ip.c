c2578353df33b45127afae943070ccd1b346d25f
4n12i <an12inakamu12a@gmail.com>
2023-08-27 20:10:40 +0900

    step8: IP datagram output

--- ip.c
+++ ip.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
 
 #include "platform.h"
 
@@ -216,6 +218,118 @@ ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 
     debugf("dev=%s, iface=%s, protocol=%u, total=%u", dev->name, ip_addr_ntop(iface->unicast, addr, sizeof(addr)), hdr->protocol, total);
     ip_dump(data, total);
+}
+
+/** 
+ * STEP8 IPデータグラムの出力
+*/
+
+/* デバイスからの送信 */
+static int 
+ip_output_device(struct ip_iface *iface, const uint8_t *data, size_t len, ip_addr_t dst)
+{
+    uint8_t hwaddr[NET_DEVICE_ADDR_LEN] = {};
+
+    if (NET_IFACE(iface)->dev->flags & NET_DEVICE_FLAG_NEED_ARP) { /* ARPによるアドレス解決が必要なデバイスのための処理 */
+        if (dst == iface->broadcast || dst == IP_ADDR_BROADCAST) { /* 宛先がブロードキャストIPアドレスの場合にはARPによるアドレス解決は行わずにそのデバイスのブロードキャストHWアドレスを使う */
+            memcpy(hwaddr, NET_IFACE(iface)->dev->broadcast, NET_IFACE(iface)->dev->alen);
+        } else {
+            errorf("arp does not implement");
+            return -1;
+        }
+    }
+
+    /* EXERCISE 8-4: */
+    return net_device_output(NET_IFACE(iface)->dev, NET_PROTOCOL_TYPE_IP, data, len, hwaddr); /* インタフェースに紐づくデバイスからIPデータグラムを送信 */
+}
+
+/* IPデータグラムの生成 */
+static ssize_t
+ip_output_core(struct ip_iface *iface, uint8_t protocol, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, uint16_t id, uint16_t offset)
+{
+    uint8_t buf[IP_TOTAL_SIZE_MAX];
+    struct ip_hdr *hdr;
+    uint16_t hlen, total;
+    char addr[IP_ADDR_STR_LEN];
+
+    hdr = (struct ip_hdr *)buf;
+
+    /* EXERCISE 8-3: IPデータグラムの生成 */
+    hlen = IP_HDR_SIZE_MIN;
+    hdr->vhl = (IP_VERSION_IPV4 << 4) | (hlen >> 2); /* オプションなし */
+    hdr->tos = 0;
+    total = hlen + len;
+    hdr->total = hton16(total);
+    hdr->id = hton16(id);
+    hdr->offset = hton16(offset);
+    hdr->ttl = 0xff; /* 255 */
+    hdr->protocol = protocol;
+    hdr->sum = 0;
+    hdr->src = src;
+    hdr->dst = dst;
+    hdr->sum = cksum16((uint16_t *)hdr, hlen, 0);
+    memcpy(hdr + 1, data, len); /* IPヘッダの直後にデータを配置する */
+
+    debugf("dev=%s, dst=%s, protocol=%u, len=%u", 
+        NET_IFACE(iface)->dev->name, ip_addr_ntop(dst, addr, sizeof(addr)), protocol, total);
+    ip_dump(buf, total);
+    return ip_output_device(iface, buf, total, dst); /* 生成したIPデータグラムを実際にデバイスから送信するための関数に渡す */
+}
+
+/* IPの出力関数 */
+static uint16_t
+ip_generate_id(void)
+{
+    static mutex_t mutex = MUTEX_INITIALIZER;
+    static uint16_t id = 128;
+    uint16_t ret;
+
+    mutex_lock(&mutex);
+    ret = id++;
+    mutex_unlock(&mutex);
+    return ret;
+}
+
+ssize_t
+ip_output(uint8_t protocol, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst)
+{
+    struct ip_iface *iface;
+    char addr[IP_ADDR_STR_LEN];
+    uint16_t id;
+
+    /* 送信先IPアドレスが指定されていない場合はエラーを返す */
+    if (src == IP_ADDR_ANY) {
+        errorf("ip routing does not implement");
+        return -1;
+    } else { /* NOTE: I'll rewrite this block later. */
+        /* EXERCISE 8-1: IPインタフェースの検索 */
+        /* 送信元IPアドレス（src）に対応するIPインタフェースを検索 */
+        iface = ip_iface_select(src);
+        if (!iface) {
+            errorf("iface not found, src=%s", ip_addr_ntop(src, addr, sizeof(addr)));
+            return -1;
+        }
+
+        /* EXERCISE 8-2: 宛先へ到達可能か確認 */
+        /* ネットワーク部が異なる or ブロードキャストアドレスでない場合は到達不能なのでエラーを返す */
+        if ((dst & iface->netmask) != (iface->unicast & iface->netmask) && dst != IP_ADDR_BROADCAST) {
+            errorf("unreachable, dst=%s", ip_addr_ntop(dst, addr, sizeof(addr)));
+            return -1;
+        }
+    }
+    /* フラグメンテーションをサポートしないので、MTUを超える場合はエラーを返す */
+    if (NET_IFACE(iface)->dev->mtu < IP_HDR_SIZE_MIN + len) {
+        errorf("too long, dev=%s, mtu=%u < %zu", 
+            NET_IFACE(iface)->dev->name, NET_IFACE(iface)->dev->mtu, IP_HDR_SIZE_MIN + len);
+        return -1;
+    }
+    id = ip_generate_id(); /* IPデータグラムのIDを採番 */
+    /* IPデータグラムを生成して出力するための関数を呼び出す */
+    if (ip_output_core(iface, protocol, data, len, iface->unicast, dst, id, 0) == -1) {
+        errorf("ip_output_core() failure");
+        return -1;
+    }
+    return len;
 }
 
 int 
