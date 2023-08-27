9fdf15d40e45c5b3eae8259f73abbb8a92b8f9d8
4n12i <an12inakamu12a@gmail.com>
2023-08-28 01:30:52 +0900

    step10: Input and validation of ICMP messages

--- Makefile
+++ Makefile
@@ -19,6 +19,7 @@ TESTS = test/step0.exe \
 		test/step7.exe \
 		test/step8.exe \
 		test/step9.exe \
+		test/step10.exe \
 
 # Options of C compiler
 CFLAGS := $(CFLAGS) -g -W -Wall -Wno-unused-parameter -iquote .
--- icmp.c
+++ icmp.c
@@ -5,15 +5,103 @@
 #include "ip.h"
 #include "icmp.h"
 
+/* ICMP header */
+struct icmp_hdr {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint32_t values;
+};
+
+/* Echo/EchoReply message */
+struct icmp_echo {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint16_t id;
+    uint16_t seq;
+};
+
+static char *
+icmp_type_ntoa(uint8_t type) {
+    switch (type) {
+    case ICMP_TYPE_ECHOREPLY:
+        return "EchoReply";
+    case ICMP_TYPE_DEST_UNREACH:
+        return "DestinationUnreachable";
+    case ICMP_TYPE_SOURCE_QUENCH:
+        return "SourceQuench";
+    case ICMP_TYPE_REDIRECT:
+        return "Redirect";
+    case ICMP_TYPE_ECHO:
+        return "Echo";
+    case ICMP_TYPE_TIME_EXCEEDED:
+        return "TimeExceeded";
+    case ICMP_TYPE_PARAM_PROBLEM:
+        return "ParameterProblem";
+    case ICMP_TYPE_TIMESTAMP:
+        return "Timestamp";
+    case ICMP_TYPE_TIMESTAMPREPLY:
+        return "TimestampReply";
+    case ICMP_TYPE_INFO_REQUEST:
+        return "InformationRequest";
+    case ICMP_TYPE_INFO_REPLY:
+        return "InformationReply";
+    }
+    return "Unknown";
+}
+
+/* デバッグ出力 */
+static void
+icmp_dump(const uint8_t *data, size_t len)
+{
+    struct icmp_hdr *hdr;
+    struct icmp_echo *echo;
+
+    flockfile(stderr);
+    hdr = (struct icmp_hdr *)data;
+    /* 全メッセージ共通のフィールド */
+    fprintf(stderr, "       type: %u (%s)\n", hdr->type, icmp_type_ntoa(hdr->type));
+    fprintf(stderr, "       code: %u\n", hdr->code);
+    fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+    switch (hdr->type) {
+    /* Echo/EchoReply の場合は詳細を出力 */
+    case ICMP_TYPE_ECHOREPLY:
+    case ICMP_TYPE_ECHO:
+        echo = (struct icmp_echo *)hdr;
+        fprintf(stderr, "         id: %u\n", ntoh16(echo->id));
+        fprintf(stderr, "        seq: %u\n", ntoh16(echo->seq));
+        break;
+    /* その他メッセージの場合は32bit値をそのまま出力 */
+    default:
+        fprintf(stderr, "     values: 0x%08x\n", ntoh32(hdr->values));
+        break;
+    }
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);
+}
+
 /* ICMPの入力関数 */
 void 
 icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface) 
 {
+    struct icmp_hdr *hdr;
     char addr1[IP_ADDR_STR_LEN];
     char addr2[IP_ADDR_STR_LEN];
 
+    /* EXERCISE 10-1: ICMPメッセージの検証 */
+    hdr = (struct icmp_hdr *)data;
+    if (len < ICMP_HDR_SIZE) {
+        errorf("icmp header length error: len=%zu < hlen=%d", len, ICMP_HDR_SIZE);
+    }
+    if (cksum16((uint16_t *)data, len, 0) != (uint16_t)0) {
+        errorf("checksum error: sum=0x%04x, verify=0x%04x", ntoh16(hdr->sum), ntoh16(cksum16((uint16_t *)hdr, ICMP_HDR_SIZE, -hdr->sum)));
+    }
+
     debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
-    debugdump(data, len);
+    icmp_dump(data, len);
 }
 
 int 
